638fb2237deae820880ca37ae5e4755d9850b7ba
John Kenneth Garcia <kengarcia.96@gmail.com>
2019-10-07 23:34:22 +0800

    Fixed logic, refactor codes

--- main/main.js
+++ main/main.js
@@ -2,57 +2,54 @@ module.exports = function main(inputs) {
     // console.log("Debug Info");
     // return 'Hello World!';
 
+    return printReceipt(inputs);
+};
 
-    function printReceipt (){
+    function printReceipt (inputs){
+        let receiptFormat;
         let summary = [];
-        let count = 0;
-        let sum = 0;
+        let totalCost = 0;
 
-        for (let i=0; i < inputs.length; i++){
+        for(let i = 0; i < inputs.length; i++){
 
             let newItem = true;
-            for (let j=0; j < summary.length; j++){
-                if (inputs[i] == summary[j].Barcode){
-                    sum +=inputs[i].Price;
-                    summary[j].Price=sum;
-                    summary[j].count++;
-                    newEntry = false;
+
+            for(let j = 0; j < summary.length; j++){
+                if(inputs[i].Barcode == summary[j].Barcode){
+                    summary[j].Count++;
+                    summary[j].TotalPrice += inputs[i].Price;
+                    newItem = false;
+                    break;
                 }
             }
-
-            if(newItem === true){
-                sum +=inputs[i].Price;
-                summary.push({Barcode: inputs[i].Barcode, Name: inputs[i].Name, Unit: inputs[i].Unit, Price: sum, count: count++}); 
-                // newItem = false;
-            }
-
-            // else{
-            //     sum +=inputs[i].Price;
-            //     summary.push({Barcode: inputs[i].Barcode, Name: inputs[i].Name, Unit: inputs[i].Unit, Price: sum, count: count++}); 
-            //     newItem = false;
-            // }
-
             
-            // else{
-            //     sum +=inputs[i].Price;
-            //     summary.push({Price: sum, count: count++}); 
-            //     newItem = false;
-            // }
-
+            if(newItem === true){
+                    summary.push({Barcode : inputs[i].Barcode, Name : inputs[i].Name, Price : inputs[i].Price, Unit : inputs[i].Unit, Count : 1, TotalPrice: inputs[i].Price});
+            }
+    
         }
 
-        return summary;
+
+        receiptFormat = '***<store earning no money>Receipt ***\n';
+
+        for (let n = 0; n < summary.length; n++){
+
+            if(summary[n].Unit == 'bottle'){
+                receiptFormat += 'Name: ' + summary[n].Name + ', Quantity: ' + summary[n].Count + ' bottles, Unit price: '+ summary[n].Price + '.00' +' (yuan), Subtotal: ' + summary[n].TotalPrice + '.00' + ' (yuan)\n';
+                totalCost += summary[n].TotalPrice;
+            }
+
+            else{   
+                receiptFormat += 'Name: ' + summary[n].Name + ', Quantity: ' + summary[n].Count + ', Unit price: '+ summary[n].Price + '.00' +' (yuan), Subtotal: ' + summary[n].TotalPrice + '.00' + ' (yuan)\n';
+                totalCost += summary[n].TotalPrice;
+            } 
+        
+        }
+    
+        receiptFormat += '----------------------\n';
+        receiptFormat += 'Total: ' + totalCost + '.00' +' (yuan)\n';
+        receiptFormat += '**********************\n';
+
+
+        return receiptFormat;
     }
-
-
-    return printReceipt();
-
-
-    // return  '***<store earning no money>Receipt ***\n' +
-    // 'Name: Coca-Cola, Quantity: 5 bottles, Unit price: 3.00 (yuan), ' + 'Subtotal: 15.00 (yuan)\n' +
-    // 'Name: Sprite, Quantity: 2 bottles, Unit price: 3.00 (yuan), Subtotal: 6.00 (yuan)\n' +
-    // 'Name: Battery, Quantity: 1, Unit price: 2.00 (yuan), Subtotal: 2.00 (yuan)\n' +
-    // '----------------------\n' +
-    // 'Total: 23.00 (yuan)\n' +
-    // '**********************\n';
-};
